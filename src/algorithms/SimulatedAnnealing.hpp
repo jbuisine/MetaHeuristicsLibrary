5809ee9178f54c4d6704b7d26de1dbfaf31b66be
Jérôme Buisine <jerome.buisine@outlook.fr>
2017-09-03 23:27:16 +0200

    SimulatedAnnealing algorithm explained

--- src/algorithms/SimulatedAnnealing.hpp
+++ src/algorithms/SimulatedAnnealing.hpp
@@ -26,16 +26,24 @@ public:
      * Simulated annealing simple implementation
      * @param nbEvaluation : number of iteration
      * @param temperature : starting temperature to decrease
-     * @param stepSize : step defined to decrease temperature
-     * @param nbPerturbation : number of elements permuted into a solution at each movement
+     * @param alpha : constant defined to decrease temperature (typical choices are between 0.8 & 0.99) 
      * @return
      */
-    C* SimulatedAnnealingSimple(int nbEvaluation, int temperature, int stepSize, int nbPerturbation){
+    C* SimulatedAnnealingSimple(int nbEvaluation, int temperature, int alpha){
 
         // Best solution to return
         C *best = new C(this->size);
 
         // TODO Implement simulated annealing algorithm
+
+        // Initialize T, T_min and alpha a constant used to decrease temperature
+
+        // 1. Generate random solution
+        // 2. Calculate cost solution defined
+        // 3. Getting randomly a neighboring solution
+        // 4. Calculate new solution's cost
+        // 5. Compare the two solutions
+        // 6. Repeat step 3 to 5 above until verified stop criterion
 
         return best;
     }
